14ab90b4ed5b1986770e80019c4842853759e682
Clebson Moura <clebson.sh@gmail.com>
2024-07-11 22:27:22 -0300

    add update ticket routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -7,9 +7,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('tickets', TicketController::class)->except(['update']);
+    Route::patch('tickets/{ticket}', [TicketController::class, 'update']);
     Route::put('tickets/{ticket}', [TicketController::class, 'replace']);
 
     Route::apiResource('authors', AuthorController::class);
+
     Route::apiResource('authors.tickets', AuthorTicketsController::class)->except(['update']);
+    Route::patch('authors/{author}/tickets/{ticket}', [AuthorTicketsController::class, 'update']);
     Route::put('authors/{author}/tickets/{ticket}', [AuthorTicketsController::class, 'replace']);
 });

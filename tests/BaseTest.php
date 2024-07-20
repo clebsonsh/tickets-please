195b3f7b88e98983a126bd106dae6b8d161510b2
Clebson Moura <clebson.sh@gmail.com>
2024-07-20 13:35:21 -0300

    add loginAs method to BaseTest

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,9 +2,32 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase;
+use Illuminate\Testing\TestResponse;
 
 abstract class BaseTest extends TestCase
 {
-    //
+    protected User $user;
+
+    /**
+     * @var array <string, string>
+     */
+    protected array $validUser = [
+        'email' => 'valid@email.com',
+        'password' => 'password',
+    ];
+
+    /**
+     * @param  array <string, string>  $userData
+     */
+    public function loginAs(array $userData): TestResponse
+    {
+        $this->user = User::factory($userData)->create();
+
+        return $this->postJson(route('login'), [
+            'email' => $userData['email'] ?? '',
+            'password' => $userData['password'] ?? '',
+        ]);
+    }
 }

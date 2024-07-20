bdacabbe7c51f6f57e72c7887ed514c7786bae29
Clebson Moura <clebson.sh@gmail.com>
2024-07-20 00:44:49 -0300

    giving name for the routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [Authcontroller::class, 'login']);
-Route::post('/register', [Authcontroller::class, 'register']);
-Route::delete('/logout', [Authcontroller::class, 'logout'])->middleware('auth:sanctum');
+Route::post('/login', [Authcontroller::class, 'login'])->name('login');
+Route::post('/register', [Authcontroller::class, 'register'])->name('register');
+Route::delete('/logout', [Authcontroller::class, 'logout'])->middleware('auth:sanctum')->name('logout');
